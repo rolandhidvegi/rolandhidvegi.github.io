159937ccedf5265d9ddc690567d756e592db0a8f
Roland Hidvegi <roland0hidvegi@gmail.com>
2026-06-01 13:56:17 +0300

    Initial upload to the redesign branch

--- script.js
+++ script.js
@@ -4,3 +4,26 @@ function toggleMenu(){
     menu.classList.toggle('open');
     icon.classList.toggle('open');
 }
+
+// Toggle image overlay on mobile/touch devices
+document.querySelectorAll('.image-container').forEach(container => {
+    container.addEventListener('click', function(e) {
+        this.classList.toggle('show-hover');
+    });
+});
+
+// Close image overlay when clicking outside
+document.addEventListener('click', function() {
+    document.querySelectorAll('.image-container').forEach(container => {
+        container.classList.remove('show-hover');
+    });
+});
+
+// Prevent link navigation when image is clicked
+// document.querySelectorAll('.project-link').forEach(link => {
+//     link.addEventListener('click', function(e) {
+//         if (e.target.closest('.image-container')) {
+//             e.preventDefault();
+//         }
+//     });
+// });
